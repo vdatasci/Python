7c706c7e7b7b1db018e502f3197589ae62714393
VossDataScience <joshvoss@outlook.com>
2016-11-09 13:06:14 -0500

    Create Random-Wikipedia-Article.py

---
+++ ETL/Web-Scraping/Random-Wikipedia-Article.py
@@ -0,0 +1,20 @@
+import requests
+from BeautifulSoup import BeautifulSoup
+import re
+
+
+url = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=xml'
+
+response = requests.get(url)
+html = response.content
+soup = BeautifulSoup(html)
+
+ids = []
+for item in soup.findAll(id=re.compile("[0-9]")):
+     id.append(item['id'])
+     
+titles = []
+for name in soup.findAll(title=re.compile(".*")):
+     titles.append(name['title'])
+
+
