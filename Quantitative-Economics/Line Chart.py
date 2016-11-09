c308e57e4115b9aed1c8ca75c765c52daf93da20
VossDataScience <joshvoss@outlook.com>
2016-11-08 23:18:25 -0500

    Create Line Chart.py

---
+++ Quantitative-Economics/Line Chart.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pylab as pl
+
+# Make an array of x values
+x = [1, 2, 3, 4, 5]
+# Make an array of y values for each x value
+y = [1, 4, 9, 16, 25]
+
+# use pylab to plot x and y
+pl.plot(x, y)
+
+# show the plot on the screen
+pl.show()
