c59c401ac4649f2c23644469ff2eb93d168c040a
VossDataScience <joshvoss@outlook.com>
2016-11-08 22:17:52 -0500

    Create INSTALL-modules.bat

---
+++ INSTALL-modules.bat
@@ -0,0 +1,61 @@
+C:\Python27\ArcGIS10.4\Scripts\pip install requests
+C:\Python27\ArcGIS10.4\Scripts\pip install BeautifulSoup
+C:\Python27\ArcGIS10.4\Scripts\pip install bs4
+C:\Python27\ArcGIS10.4\Scripts\pip install sqlalchemy
+C:\Python27\ArcGIS10.4\Scripts\pip install numpy
+C:\Python27\ArcGIS10.4\Scripts\pip install pandas
+C:\Python27\ArcGIS10.4\Scripts\pip install pygal
+C:\Python27\ArcGIS10.4\Scripts\pip install matplotlib
+C:\Python27\ArcGIS10.4\Scripts\pip install flask
+C:\Python27\ArcGIS10.4\Scripts\pip install django
+C:\Python27\ArcGIS10.4\Scripts\pip install Seaborn
+C:\Python27\ArcGIS10.4\Scripts\pip install pyopt
+C:\Python27\ArcGIS10.4\Scripts\pip install fuzzywuzzy
+C:\Python27\ArcGIS10.4\Scripts\pip install blockchain
+C:\Python27\ArcGIS10.4\Scripts\pip install adodbapi
+C:\Python27\ArcGIS10.4\Scripts\pip install twisted
+C:\Python27\ArcGIS10.4\Scripts\pip install xlrd
+C:\Python27\ArcGIS10.4\Scripts\pip install SimPy
+C:\Python27\ArcGIS10.4\Scripts\pip install SymPy
+C:\Python27\ArcGIS10.4\Scripts\pip install statistics
+C:\Python27\ArcGIS10.4\Scripts\pip install GeoPy
+C:\Python27\ArcGIS10.4\Scripts\pip install scikit-fuzzy
+C:\Python27\ArcGIS10.4\Scripts\pip install praw
+C:\Python27\ArcGIS10.4\Scripts\pip install lxml 
+C:\Python27\ArcGIS10.4\Scripts\pip install html5lib
+C:\Python27\ArcGIS10.4\Scripts\pip install PyPI
+C:\Python27\ArcGIS10.4\Scripts\pip install indeed
+C:\Python27\ArcGIS10.4\Scripts\pip install easygui
+C:\Python27\ArcGIS10.4\Scripts\pip install TKinter
+C:\Python27\ArcGIS10.4\Scripts\pip install ykMessageBox
+C:\Python27\ArcGIS10.4\Scripts\pip install cgi
+C:\Python27\ArcGIS10.4\Scripts\pip install cx_Freeze
+C:\Python27\ArcGIS10.4\Scripts\pip install wkhtmltopdf
+C:\Python27\ArcGIS10.4\Scripts\pip install pdfkit
+C:\Python27\ArcGIS10.4\Scripts\pip install Pyglet
+C:\Python27\ArcGIS10.4\Scripts\pip install geoplotlib
+C:\Python27\ArcGIS10.4\Scripts\pip install Plotly
+C:\Python27\ArcGIS10.4\Scripts\pip install Gleam
+C:\Python27\ArcGIS10.4\Scripts\pip install bokeh
+C:\Python27\ArcGIS10.4\Scripts\pip install vincent
+C:\Python27\ArcGIS10.4\Scripts\pip install folium
+C:\Python27\ArcGIS10.4\Scripts\pip install graph-tools
+C:\Python27\ArcGIS10.4\Scripts\pip install pyscreenshot
+C:\Python27\ArcGIS10.4\Scripts\pip install Pillow
+
+C:\Python27\ArcGIS10.4\Scripts\pip install Pycluster
+
+C:\Python27\ArcGIS10.4\Scripts\pip install NLTK
+
+
+C:\Python27\ArcGIS10.4\Scripts\pip install quantecon
+
+C:\Python27\ArcGIS10.4\Scripts\pip install Selenium
+
+
+
+
+
+
+
+::https://pypi.python.org/pypi/?
