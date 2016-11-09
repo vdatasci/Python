d39c11aad3728440306c15544d199b3edec5f446
VossDataScience <joshvoss@outlook.com>
2016-11-08 22:29:01 -0500

    Create Basic Text Processing.py

---
+++ ETL/Basic Text Processing.py
@@ -0,0 +1,40 @@
+### Print Text File contents ###
+print(open("F:\Temp Files\TempFile.txt","r").read())
+
+###
+### Read lines of Text File ###
+file=open('filename.txt','rt')
+while true:
+	line=file.readline()
+	#do something
+file.close()
+
+###
+### Print elements together of two Arrays ###
+list1 = ['a', 'b', 'c', 'd']
+list2 = ['p', 'q', 'r', 's']
+
+for x, y in zip(list1,list2):
+print x, y
+
+###
+### String to Array ###
+numstr = '1 2 3 4'
+result = map(lambda x:int(x) ,numstr.split())
+result
+#raw_input()
+
+###
+### Concat ###
+a = ["Code", "mentor", "Python", "Developer"] 
+print " ".join(a)
+
+###
+### Convert to single List Array ###
+import itertools 
+
+a = [[1, 2], [3, 4], [5, 6]]
+list(itertools.chain.from_iterable(a))
+
+###
+
