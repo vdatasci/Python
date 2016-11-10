fba3f0ad264697951a963865abb3b4aec8425ad4
VossDataScience <joshvoss@outlook.com>
2016-11-10 10:57:06 -0500

    Create Fuzzy-Search-List-for-Value-and-Index.py

---
+++ ETL/Fuzzy-Search-List-for-Value-and-Index.py
@@ -0,0 +1,17 @@
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import re
+
+
+choices = ['Hit', 'Stay', 'Double Down']
+
+say = raw_input("Hit? Stay? Double Down? ")
+
+m = re.search('(?<=\(\').*(?=\'\,)', str(process.extract(say, choices, limit=1))).group(0)
+n = choices.index(re.search('(?<=\(\').*(?=\'\,)', str(process.extract(say, choices, limit=1))).group(0))
+
+print m
+print n
+
+
+raw_input()
