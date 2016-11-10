169791dc92c0e6b06bf24ca1efbac206cd08f7f4
VossDataScience <joshvoss@outlook.com>
2016-11-10 12:40:25 -0500

    Update Matrix-Regex-Fuzzy-Search-Index-and-Value.py

--- ETL/Matrix-Regex-Fuzzy-Search-Index-and-Value.py
+++ ETL/Matrix-Regex-Fuzzy-Search-Index-and-Value.py
@@ -22,3 +22,21 @@ j = int(re.search(' [0-9]+', str(A.shape)).group(0))
 for row in range(i):
     for col in range(j):
             print(A[row,col])
+
+
+for row in range(i):
+    for col in range(j):
+            lst.append(A[row,col])
+
+lst2 = []
+for o in range(i+j):
+    lst2.append(fuzz.partial_ratio(srch,lst[o]))
+
+maxlst2index = lst2.index(max(lst2))
+lst[maxlst2index]
+
+np.where(A==lst[maxlst2index])
+
+
+#m = re.search('(?<=\(\').*(?=\'\,)', str(process.extract(srch, A, limit=1))).group(0)
+#n = choices.index(re.search('(?<=\(\').*(?=\'\,)', str(process.extract(say, choices, limit=1))).group(0))
