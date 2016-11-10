59e5f4b0697b10106176fb485502447c43225eac
VossDataScience <joshvoss@outlook.com>
2016-11-10 08:42:28 -0500

    Create Straight-Line-Function.py

---
+++ Quantitative-Economics/Lines/Straight-Line-Function.py
@@ -0,0 +1,18 @@
+def straight_line(gradient, x, constant):
+''' returns y coordinate of a straight line gradient * x + constant'''
+  return gradient*x + constant
+  
+  
+#for x in range(10):
+#     print(x, straight_line(2,x,-3))
+#
+#(0, -3)
+#(1, -1)
+#(2, 1)
+#(3, 3)
+#(4, 5)
+#(5, 7)
+#(6, 9)
+#(7, 11)
+#(8, 13)
+#(9, 15)
