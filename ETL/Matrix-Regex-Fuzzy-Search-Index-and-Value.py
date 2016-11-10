f4c2ae40a76358d035881b19f0e32a4fcc1a7a47
VossDataScience <joshvoss@outlook.com>
2016-11-10 13:03:35 -0500

    Update Matrix-Regex-Fuzzy-Search-Index-and-Value.py

--- ETL/Matrix-Regex-Fuzzy-Search-Index-and-Value.py
+++ ETL/Matrix-Regex-Fuzzy-Search-Index-and-Value.py
@@ -5,24 +5,11 @@ import numpy as np
 
 
 A = np.matrix([['hip hop','mixcloud'],['music','jammin']])
-A
-
 srch = raw_input('Whatchu searchin? ')
-
-#process.extract(srch, A, limit=1)
-
-#process.extract(srch, A[0,0], limit=1)
-
-#A.shape
 
 #matrix dimension(i,j)
 i = int(re.search('[0-9]+', str(A.shape)).group(0))
 j = int(re.search(' [0-9]+', str(A.shape)).group(0))
-
-#for row in range(i):
-#    for col in range(j):
-#            print(A[row,col])
-
 
 for row in range(i):
     for col in range(j):
@@ -33,10 +20,6 @@ for o in range(i+j):
     lst2.append(fuzz.partial_ratio(srch,lst[o]))
 
 maxlst2index = lst2.index(max(lst2))
+
 lst[maxlst2index]
-
 np.where(A==lst[maxlst2index])
-
-
-#m = re.search('(?<=\(\').*(?=\'\,)', str(process.extract(srch, A, limit=1))).group(0)
-#n = choices.index(re.search('(?<=\(\').*(?=\'\,)', str(process.extract(say, choices, limit=1))).group(0))
