f1565a2200125fc984f17c8e3b0edaff2345cb1b
VossDataScience <joshvoss@outlook.com>
2016-11-11 13:16:45 -0500

    Update Does-URL-Website-Exist.py

--- ETL/Does-URL-Website-Exist.py
+++ ETL/Does-URL-Website-Exist.py
@@ -4,3 +4,13 @@ if (code / 100 >= 4):
    print "Nothing there."
 
 code
+
+
+
+
+
+#from urllib2 import urlopen
+#url = 'http://uconn.edu/yo'
+#f = urllib.urlopen(url)
+#if f.code==200:
+#    fxst = 'exists'
