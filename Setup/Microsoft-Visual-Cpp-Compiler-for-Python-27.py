565f9387977dd5da04c7eb93bcc2a623573522b6
VossDataScience <joshvoss@outlook.com>
2016-11-12 16:52:49 -0500

    Create Microsoft-Visual-Cpp-Compiler-for-Python-27.py

---
+++ Setup/Microsoft-Visual-Cpp-Compiler-for-Python-27.py
@@ -0,0 +1,9 @@
+#Directs a to Microsoft Visual C++ Compiler for Python 2.7 downlaod webpage.
+
+import webbrowser
+import string
+
+url = 'http://www.microsoft.com/en-us/download/confirmation.aspx?id=44266'
+webbrowser.open(url)
+
+#https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi
