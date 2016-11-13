9aba0e72e778a1fea815b49ccdfd522aa074a6a1
VossDataScience <joshvoss@outlook.com>
2016-11-13 16:21:19 -0500

    Update and rename vSQLite3.py to SQLite3-example1.py

---
+++ Databases/SQLite3-example1.py
@@ -0,0 +1,16 @@
+import sqlite3
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+
+c.execute('''
+          CREATE TABLE address
+          (id INTEGER PRIMARY KEY ASC, street_name varchar(250), street_number varchar(250),
+           post_code varchar(250) NOT NULL, person_id INTEGER NOT NULL,
+           FOREIGN KEY(person_id) REFERENCES person(id))
+          ''')
+c.execute('''
+          INSERT INTO address VALUES(1, 'python road', '1', '00000', 1)
+          ''')
+ 
+conn.commit()
+conn.close()
