74c9bb5f3dbac6a573abacc5fe8082a32574c60c
VossDataScience <joshvoss@outlook.com>
2016-11-13 13:19:53 -0500

    Create vSQLite3.py

---
+++ Databases/vSQLite3.py
@@ -0,0 +1,34 @@
+import re
+import sqlite3
+import numpy as np
+import pandas as pd
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+def dataBase(db)
+    conn = sqlite3.connect(db+'.db')
+
+def sql(statement)
+    conn.cursor().execute(statment)
+
+c.execute('''
+          CREATE TABLE person
+          (id INTEGER PRIMARY KEY ASC, name varchar(250) NOT NULL)
+          ''')
+c.execute('''
+          CREATE TABLE address
+          (id INTEGER PRIMARY KEY ASC, street_name varchar(250), street_number varchar(250),
+           post_code varchar(250) NOT NULL, person_id INTEGER NOT NULL,
+           FOREIGN KEY(person_id) REFERENCES person(id))
+          ''')
+
+c.execute('''
+          INSERT INTO person VALUES(1, 'pythoncentral')
+          ''')
+c.execute('''
+          INSERT INTO address VALUES(1, 'python road', '1', '00000', 1)
+          ''')
+
+conn.commit()
+conn.close()
+raw_input()
