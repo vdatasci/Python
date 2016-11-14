e8f22f34d6ff2fb88563a3422b1130354920b0c0
VossDataScience <joshvoss@outlook.com>
2016-11-14 11:03:38 -0500

    Update Arc-Length.py

--- Maths/Arc-Length.py
+++ Maths/Arc-Length.py
@@ -6,25 +6,20 @@ class Arc(object):
     def __init__(self, chord, height):
         self.chord = float(chord)
         self.height = float(height)
-
     def _asin(self, leg, hypotenuse, degrees = True):
         radians = math.asin(leg/hypotenuse)
         if not degrees:
             return radians
         return math.degrees(radians)
-
     def _radius(self):
         return self.chord ** 2 / (8 * self.height) + self.height / 2
-
     def _circumference(self):
         return 2 * math.pi * self.radius
-
     def _angle(self):
         return 2 * self._asin(self.chord / 2, self.radius)
-
     def getLength(self):
         self.radius = self._radius()
-        return self._angle() * self._circumference() / 360 
+        return self._angle() * self._circumference() / 360
 
 if __name__ == '__main__':
         arc = Arc(100, 10)
