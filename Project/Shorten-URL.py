bff46e2446ea4feb364a42ad0042a843ab5cd84c
VossDataScience <joshvoss@outlook.com>
2016-11-14 16:27:41 -0500

    Update Shorten-URL.py

--- Project/Shorten-URL.py
+++ Project/Shorten-URL.py
@@ -2,7 +2,6 @@ import requests
 
 tokenBitly = '9b66f572ea565ad186c0a78836113264fa8d051a'
 
-#URL shortening is crucial given twitter 140 characters restriction
 def shorten_url(longUrl):
     url="https://api-ssl.bitly.com/v3/shorten?access_token="+tokenBitly+"&longUrl="+longUrl
     b = requests.get(url)
