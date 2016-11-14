af9bb1fb68c56c07a628c9bb258457030e35eb97
VossDataScience <joshvoss@outlook.com>
2016-11-14 13:29:12 -0500

    Create Sort-By-Length.py

---
+++ ETL/Sort-By-Length.py
@@ -0,0 +1,11 @@
+def sort_by_length(words):
+    t=[]
+    for word in words:
+        t.append((len(word), word))
+    
+    t.sort(reverse=True)
+    
+    res=[]
+    for length, word in t:
+        res.append(word)
+    return res
