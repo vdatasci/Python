b2c02038e79e70c7721e4fea0229dfe6a305a1d2
VossDataScience <joshvoss@outlook.com>
2016-11-15 10:31:16 -0500

    Update Find-All-Files-Within-and-Beneath-Directory.py

--- ETL/Find-All-Files-Within-and-Beneath-Directory.py
+++ ETL/Find-All-Files-Within-and-Beneath-Directory.py
@@ -7,5 +7,7 @@ configfiles = [os.path.join(dirpath, f)
     for dirpath, dirnames, files in os.walk(path)
     for f in fnmatch.filter(files, '*.txt')]
 
+
+print 'Size:', '\t', 'FileName:', '\t', 'Date Created:'
 for file in configfiles:
-    print(file)
+    print os.stat(file).st_size, '\t', file, '\t', os.stat(file).st_birthtime
