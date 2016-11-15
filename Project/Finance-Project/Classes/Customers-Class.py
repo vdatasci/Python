977a541416466bf2c9617a7885ba7f0fa0e407bb
VossDataScience <joshvoss@outlook.com>
2016-11-15 16:55:42 -0500

    Create Customers-Class.py

---
+++ Project/Finance-Project/Classes/Customers-Class.py
@@ -0,0 +1,23 @@
+class Customer(object):
+    def __init__(self, name, balance=0.0):
+        self.name = name
+        self.balance = balance
+    
+    def withdrawl(self, amount):
+        if int(amount)>self.balance:
+            raise RuntimeError('Amount greater than available balance.')
+        self.balance -= int(amount)
+        return self.balance
+    
+    def deposit(self, amount):
+        self.balance += int(amount)
+        return self.balance
+
+
+jv = Customer('Josh Voss', 50)
+
+jv.name
+jv.balance
+jv.withdrawl(10.0)
+jv
+jv.balance
