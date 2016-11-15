5834dbcb849a701bef94cf203c0b2ea9c3beb984
VossDataScience <joshvoss@outlook.com>
2016-11-14 22:14:18 -0500

    Update Fuzzy-Regex-Searching-on-a-Matrix-to-return-the-Index-and-Value.py

--- ETL/Fuzzy-Regex-Searching-on-a-Matrix-to-return-the-Index-and-Value.py
+++ ETL/Fuzzy-Regex-Searching-on-a-Matrix-to-return-the-Index-and-Value.py
@@ -3,22 +3,15 @@ from fuzzywuzzy import process
 import re
 import numpy as np
 
-
 A = np.matrix([['hip hop','mixcloud'],['music','jammin']])
 srch = raw_input('Whatchu searchin? ')
-
-#matrix dimension(i,j)
 i = int(re.search('[0-9]+', str(A.shape)).group(0))
 j = int(re.search(' [0-9]+', str(A.shape)).group(0))
-
 for row in range(i):
     for col in range(j):
             lst.append(A[row,col])
-
 for o in range(i+j):
     lst2.append(fuzz.partial_ratio(srch,lst[o]))
-
 maxlst2index = lst2.index(max(lst2))
-
 srch_rslt = lst[maxlst2index]
 np.where(A==srch_rslt)
