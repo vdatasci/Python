7d49aac455ecca722d46a11718bbd54b2b92b5b7
VossDataScience <joshvoss@outlook.com>
2016-11-15 21:38:38 -0500

    Update Customers-Class.py

--- Project/Finance-Project/Classes/Customers-Class.py
+++ Project/Finance-Project/Classes/Customers-Class.py
@@ -3,7 +3,7 @@ class Customer(object):
     def __init__(self, name, balance=0.0):
         self.name = name
         self.balance = balance
-        self.members.append(name + ', ' + balance + ', ')
+        self.members.append(str(name) + ', ' + str(balance) + ', ')
     
     def withdrawl(self, amount):
         if int(amount)>self.balance:
