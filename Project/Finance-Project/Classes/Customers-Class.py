c4d10ba971ce228befb518cd42167ca9da623060
VossDataScience <joshvoss@outlook.com>
2016-11-15 22:59:27 -0500

    Update Customers-Class.py

--- Project/Finance-Project/Classes/Customers-Class.py
+++ Project/Finance-Project/Classes/Customers-Class.py
@@ -3,17 +3,17 @@ class Customer(object):
     def __init__(self, name, balance=0.0):
         self.name = name
         self.balance = balance
-        self.members.append(str(name) + ', ' + str(balance) + ', ')
-    
+        self.members.append(str(name) + ', ' + str(balance) + ', ')   
     def withdrawl(self, amount):
         if int(amount)>self.balance:
             raise RuntimeError('Amount greater than available balance.')
         self.balance -= int(amount)
         return self.balance
-    
     def deposit(self, amount):
         self.balance += int(amount)
         return self.balance
+    def clear(self):
+        members = []
 
 class logger:
     def __init__(self):
