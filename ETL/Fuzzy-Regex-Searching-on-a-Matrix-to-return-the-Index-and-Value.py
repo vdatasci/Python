6b386c0b54df791c7b8b4f27c45e5dcfb4696fc6
VossDataScience <joshvoss@outlook.com>
2016-11-16 12:19:44 -0500

    Update Fuzzy-Regex-Searching-on-a-Matrix-to-return-the-Index-and-Value.py

--- ETL/Fuzzy-Regex-Searching-on-a-Matrix-to-return-the-Index-and-Value.py
+++ ETL/Fuzzy-Regex-Searching-on-a-Matrix-to-return-the-Index-and-Value.py
@@ -16,9 +16,11 @@ for row in range(i):
     for col in range(j):
             lst.append(A[row,col])
 
+            
 for o in range(i+j):
     lst2.append(fuzz.partial_ratio(srch,lst[o]))
 
+    
 maxlst2index = lst2.index(max(lst2))
 srch_rslt = lst[maxlst2index]
 print(np.where(A==srch_rslt))
