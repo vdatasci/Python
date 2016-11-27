34efcb4512d7687c9d463857390a45a00d947297
VossDataScience <joshvoss@outlook.com>
2016-11-27 18:35:18 -0500

    Update Excel_to_Python_DataFrame_Table.py

--- DataFrames/Excel_to_Python_DataFrame_Table.py
+++ DataFrames/Excel_to_Python_DataFrame_Table.py
@@ -1,5 +1,18 @@
+#Excel to Python Conversion:
 from __future__ import print_function
 import pandas as pd
 import numpy as np
 df = pd.read_excel("P:\sd.xlsx")
 df.head()
+
+#Pivot Table:
+sales_report = pd.pivot_table(df, index=["Manager", "Rep", "Product"], values=["Price", "Quantity"],
+                           aggfunc=[np.sum, np.mean], fill_value=0)
+sales_report.head()
+
+#Descriptive Statistics:
+print(df[df["Product"]=="CPU"]["Quantity"].mean())
+print(df[df["Product"]=="CPU"]["Price"].mean())
+print(df[df["Product"]=="Software"]["Quantity"].mean())
+print(df[df["Product"]=="Software"]["Price"].mean())
+
