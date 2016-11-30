f32635960b6fa89954ad57914154083f57163256
VossDataScience <joshvoss@outlook.com>
2016-11-29 23:26:06 -0500

    Update Example1.py

--- Project/Mechanize/Example1.py
+++ Project/Mechanize/Example1.py
@@ -4,12 +4,11 @@ br = Browser()
 br.open('http://www.city-data.com/')
 
 br.find_link(text='CT')
-
 req = br.click_link(text='CT')
 br.open(req)
-
 print br.geturl()
 
+#browser.select_form(nr = 0)
 f = br.retrieve('http://pics3.city-data.com/images/data-stats-new.png')[0]
 print f
 fh = open(f)
