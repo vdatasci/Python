c59d8886248d7c35b0cf7ecea893a7e2c4038817
VossDataScience <joshvoss@outlook.com>
2016-11-29 23:15:55 -0500

    Update Example1.py

--- Project/Mechanize/Example1.py
+++ Project/Mechanize/Example1.py
@@ -5,7 +5,7 @@ br.open('http://www.city-data.com/')
 
 br.find_link(text='CT')
 
-req = br.click_link(text='Weekend codes')
+req = br.click_link(text='CT')
 br.open(req)
 print br.response().read()
 print br.geturl()
