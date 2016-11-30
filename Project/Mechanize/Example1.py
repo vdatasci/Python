5e78581bd6f437069c44765d34d5cd68b6cb2fd1
VossDataScience <joshvoss@outlook.com>
2016-11-29 23:29:09 -0500

    Update Example1.py

--- Project/Mechanize/Example1.py
+++ Project/Mechanize/Example1.py
@@ -1,4 +1,5 @@
 from mechanize import Browser
+import webbrowser
 
 br = Browser()
 br.open('http://www.city-data.com/')
@@ -7,6 +8,9 @@ br.find_link(text='CT')
 req = br.click_link(text='CT')
 br.open(req)
 print br.geturl()
+clickedurl = br.geturl()
+
+webbrowser.open(clickedurl,new=1)
 
 #browser.select_form(nr = 0)
 f = br.retrieve('http://pics3.city-data.com/images/data-stats-new.png')[0]
