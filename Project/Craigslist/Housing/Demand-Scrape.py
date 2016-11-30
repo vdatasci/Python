ed1b8eb5d2c63594ed73e948244b875003a404c9
VossDataScience <joshvoss@outlook.com>
2016-11-30 01:32:38 -0500

    Update Demand-Scrape.py

--- Project/Craigslist/Housing/Demand-Scrape.py
+++ Project/Craigslist/Housing/Demand-Scrape.py
@@ -7,14 +7,14 @@ response = requests.get(url)
 html = response.content
 soup = BeautifulSoup(html)
 
+listingtaglist = []
+listingtags = soup.findAll("li", {"class" : "result-row"})
+for listings in listingtags:
+    listingtaglist.append(listings.text)
+    print listings.text
+
 pricetagslist = []
 pricetags = soup.findAll("span", {"class" : ["result-price"]})
 for prices in pricetags:
     pricetagslist.append(prices.text)
     print prices.text
-
-listingtaglist = []
-listingtags = soup.findall("a", {"class":["result-title hdrlnk"]})
-for listings in listingtags:
-    listingtaglist.append(listings.text)
-    print listings.text
