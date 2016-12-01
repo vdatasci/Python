2ff7c362b0251f3221d08b0f39d04b7f4d256b95
VossDataScience <joshvoss@outlook.com>
2016-11-30 20:53:46 -0500

    Update Demand-Scrape.py

--- Project/Craigslist/Housing/Demand-Scrape.py
+++ Project/Craigslist/Housing/Demand-Scrape.py
@@ -3,6 +3,16 @@ import requests
 import numpy
 import re
 
+
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+
 url = 'https://grandrapids.craigslist.org/search/hsw'
 response = requests.get(url)
 html = response.content
