7279e69b3c37c88560e60e9b1b492accf7f932d4
VossDataScience <joshvoss@outlook.com>
2016-12-03 15:10:25 -0500

    Update Fuzzy-c-means-(FCM).py

--- Quantitative-Economics/Statistics/Fuzzy-c-means-(FCM).py
+++ Quantitative-Economics/Statistics/Fuzzy-c-means-(FCM).py
@@ -1,11 +1,3 @@
-#This code is from the skfuzzy Document. I will be creating a Quantitative Economics example soon.
-
-#Fuzzy c-means (FCM) is a data clustering technique in which a dataset is grouped into n clusters 
-#with every datapoint in the dataset belonging to every cluster to a certain degree.
-
-#In marketing, customers can be grouped into fuzzy clusters based on their 
-#needs, brand choices, psycho-graphic profiles, or other marketing related partitions.
-
 from __future__ import division, print_function
 import numpy as np
 import matplotlib.pyplot as plt
@@ -35,14 +27,6 @@ for label in range(3):
             color=colors[label])
 
 
-tplotlib.lines.Line2D object at 0x0371BFB0>]
-tplotlib.lines.Line2D object at 0x03728150>]
-tplotlib.lines.Line2D object at 0x037284B0>]
-
-
 ax0.set_title('Test data: 200 points x3 clusters.')
-plotlib.text.Text object at 0x03703E50>
-
-
 
 plt.show()
