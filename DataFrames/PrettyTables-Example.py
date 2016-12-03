7fff7f6269594e184da22f6c59ad4a4c30146133
VossDataScience <joshvoss@outlook.com>
2016-12-03 14:58:30 -0500

    Update PrettyTables-Example.py

--- DataFrames/PrettyTables-Example.py
+++ DataFrames/PrettyTables-Example.py
@@ -1,9 +1,10 @@
 from prettytable import PrettyTable
 import csv
 
-with open('P:\\Python\\Data\\randomdata.txt','r') as fp:
-    reader = csv.DictReader(fp, dialect='excel-tab')
+with open('P:\\Python\\Data\\randomdata.txt','r') as f:
+    reader = csv.DictReader(f, dialect='excel-tab')
     ptable = PrettyTable(reader.fieldnames)
     for rowdict in reader:
         ptable.add_row([rowdict[fn] for fn in reader.fieldnames])
-    print(ptable)
+    with open('P:\\TempFile.txt', 'w') as fp:
+        fp.write(ptable)
