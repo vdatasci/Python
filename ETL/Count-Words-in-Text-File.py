4c859144ba4fbe1681589c44996abd47f8539465
VossDataScience <joshvoss@outlook.com>
2016-12-03 14:22:44 -0500

    Update Count-Words-in-Text-File.py

--- ETL/Count-Words-in-Text-File.py
+++ ETL/Count-Words-in-Text-File.py
@@ -1,8 +1,10 @@
 import re
 from collections import Counter
 
+
 with open('P:\commonwords.txt') as f:
     passage = f.read()
+
 
 words = re.findall(r'\w+', passage)
 cap_words = [word.upper() for word in words]
