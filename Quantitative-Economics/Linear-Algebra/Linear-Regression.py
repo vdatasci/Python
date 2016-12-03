f34f77b7a94808fad0129b3a7854daf20f9387cc
VossDataScience <joshvoss@outlook.com>
2016-12-03 17:20:29 -0500

    Create Linear-Regression.py

---
+++ Quantitative-Economics/Linear-Algebra/Linear-Regression.py
@@ -0,0 +1,11 @@
+from numpy import arange,array,ones,linalg
+from pylab import plot,show
+
+xi = arange(0,9)
+A = array([xi, ones(9)])
+
+y = [19, 20, 20.5, 21.5, 22, 23, 23, 25.5, 24]
+w = linalg.lstsq(A.T, y)[0]
+
+line = w[0]*xi+w[1]
+plot(xi, line, 'r-', xi, y, 'o')
