f3dcc24c767853e8ab59bf8ba980211329ac4036
VossDataScience <joshvoss@outlook.com>
2016-12-03 08:13:24 -0500

    Update Linear-Search.py

--- ETL/Linear-Search.py
+++ ETL/Linear-Search.py
@@ -3,3 +3,12 @@ def lsearch(my_list, term):
         if my_list[i] == term:
             return i
         return None
+    
+my_list = [‘element 1’, ‘item two’, ‘element three’, ‘item 4’]
+term = ‘item two’
+
+lsearch(my_list,  term)
+item two
+
+position = my_list.index(term)
+1
