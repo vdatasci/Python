4482ac2f141ae43042dc4e1152fe49a1e9452782
VossDataScience <joshvoss@outlook.com>
2016-12-04 18:53:22 -0500

    Update Regex-Linear-Search.py

--- ETL/Regex-Linear-Search.py
+++ ETL/Regex-Linear-Search.py
@@ -1,4 +1,5 @@
 def regularsearch(my_list, term):
+    import re
     my_list_pos = [i for i, x in enumerate(my_list) if re.search(term, x)]
     result = str(my_list[my_list_pos])
     regularsearch = result
