6d119526a28148e5472fdc4a60ea243affabc80f
VossDataScience <joshvoss@outlook.com>
2016-12-04 17:09:49 -0500

    Create Lookup.py

---
+++ Project/Runescape/Item-Search/Lookup.py
@@ -0,0 +1,20 @@
+from BeautifulSoup import BeautifulSoup
+import mechanize
+import requests
+import re
+
+
+browser = mechanize.Browser()
+browser.set_handle_robots(False)
+browser.open("http://services.runescape.com/m=itemdb_oldschool/top100?list=0")
+
+
+for form in browser.forms():
+    print "Form name:", form.name
+    print form
+
+for control in browser.form.controls:
+    print control
+
+
+browser.form = list(browser.forms())[0]
