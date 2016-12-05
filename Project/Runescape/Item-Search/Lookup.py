06de4a863b6a76a535f5c80077e708efc2c505d9
VossDataScience <joshvoss@outlook.com>
2016-12-04 19:55:13 -0500

    Update Lookup.py

--- Project/Runescape/Item-Search/Lookup.py
+++ Project/Runescape/Item-Search/Lookup.py
@@ -40,6 +40,16 @@ links = table.findAll('a')
 for link in links:
     print links['href']
 
+for row in table.tbody.findAll('tr'):
+    first_column = row.findAll('th')[0].contents
+    third_column = row.findAll('td')[2].contents
+    print first_column, third_column
+
+
+
+
+
+
 
 rows=list()
 for row in table.findAll("tr"):
