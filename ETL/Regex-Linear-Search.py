6b6ea108827185ec52b64a736635660b80c7f7db
VossDataScience <joshvoss@outlook.com>
2016-12-04 19:16:01 -0500

    Update Regex-Linear-Search.py

--- ETL/Regex-Linear-Search.py
+++ ETL/Regex-Linear-Search.py
@@ -1,4 +1,4 @@
 def regularsearch(my_list, pattern):
     import re
     return re.findall(pattern, ','.join(my_list))
-    #[i for i, x in enumerate(a) if re.search('\w+', x)]
+    return [i for i, x in enumerate(my_list) if re.search(pattern, x)]
