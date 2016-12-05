ac9576523a859276d56917c958fcbb622a6a2f00
VossDataScience <joshvoss@outlook.com>
2016-12-04 19:20:07 -0500

    Update Regex-Linear-Search.py

--- ETL/Regex-Linear-Search.py
+++ ETL/Regex-Linear-Search.py
@@ -1,4 +1,4 @@
 def regularsearch(my_list, pattern):
     import re
     return re.findall(pattern, ','.join(my_list))
-    [i for i, x in enumerate(my_list) if re.search(pattern, x)]
+    return [i for i, x in enumerate(my_list) if re.search(pattern, x)]
