630228fa33def711da695e7a356ecd4abadfcd47
VossDataScience <joshvoss@outlook.com>
2016-12-04 19:15:27 -0500

    Update Regex-Linear-Search.py

--- ETL/Regex-Linear-Search.py
+++ ETL/Regex-Linear-Search.py
@@ -1,4 +1,4 @@
 def regularsearch(my_list, pattern):
     import re
-    re.findall(pattern, ','.join(my_list))
+    return re.findall(pattern, ','.join(my_list))
     #[i for i, x in enumerate(a) if re.search('\w+', x)]
