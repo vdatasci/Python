d23b093e5f975a06fe2d12cfe6260cd388461197
VossDataScience <joshvoss@outlook.com>
2016-12-04 19:16:49 -0500

    Update Regex-Linear-Search.py

--- ETL/Regex-Linear-Search.py
+++ ETL/Regex-Linear-Search.py
@@ -1,4 +1,4 @@
 def regularsearch(my_list, pattern):
     import re
     return re.findall(pattern, ','.join(my_list))
-    return [i for i, x in enumerate(my_list) if re.search(pattern, x)]
+    [i for i, x in enumerate(my_list) if re.search(pattern, x)]
