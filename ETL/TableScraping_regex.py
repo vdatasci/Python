0928d746e8e7e4f7552b093c15628d2727fb688f
VossDataScience <joshvoss@outlook.com>
2016-12-05 23:43:25 -0500

    Create TableScraping_regex.py

---
+++ ETL/TableScraping_regex.py
@@ -0,0 +1,45 @@
+from BeautifulSoup import BeautifulSoup
+import mechanize
+import requests
+import re
+
+
+url= 'http://www.w3schools.com/html/html_tables.asp'
+
+response = requests.get(url)
+html = response.content
+soup = BeautifulSoup(html)
+
+
+table = soup.find('table')
+links = table.findAll('a')
+
+linklist = []
+for link in links:
+    linklist.append(link['href'])
+
+
+headers = []
+for header in table.findAll('th'):
+    headers.append(header.text.encode('utf8'))
+
+
+rows = []
+for row in table.findAll('tr'):
+    rows.append([val.text.encode('utf8') for val in row.findAll('td')])
+
+
+del rows[0]
+
+
+with open('P:\TempFile.txt', 'w') as f:
+    f.write(str(', '.join(headers)))
+
+
+with open("P:\TempFile.txt", "a") as f:
+    for r in rows:
+        f.write(str(', '.join(r)) + '\n')
+        print(re.compile('(fire)\w+').match(j).group())
+
+
+
