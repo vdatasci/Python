ccf52d49a084ea4abc4e812e07511aab13c32bb8
VossDataScience <joshvoss@outlook.com>
2016-12-06 14:43:10 -0500

    Update Youtube-Trends.py

--- Project/Youtube-Trends.py
+++ Project/Youtube-Trends.py
@@ -34,3 +34,33 @@ for v in vlist:
         soup = BeautifulSoup(html)
         vtitles.append(str((soup.findAll('title'))).replace('[<title>','').replace('</title>]',''))
 
+
+
+
+
+
+
+from collections import Counter
+words = re.findall(r'\w+', str(vtitles))
+cap_words = [word.upper() for word in words]
+word_counts = Counter(cap_words)
+word_counts
+
+
+
+
+
+
+
+
+from collections import Counter
+import numpy as np
+import matplotlib.pyplot as plt
+words = re.findall(r'\w+', str(vtitles))
+cap_words = [word.upper() for word in words]
+labels, values = zip(*Counter(cap_words).most_common())
+indexes = np.arange(len(labels[:5]))
+width = 1
+plt.bar(indexes, values, width)
+plt.xticks(indexes + width * 0.5, labels)
+plt.show()
