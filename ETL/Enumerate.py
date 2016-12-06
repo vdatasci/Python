7164677df2cf431a1ad5c4ea72d3933b0064e82e
VossDataScience <joshvoss@outlook.com>
2016-12-06 08:06:04 -0500

    Create Enumerate.py

---
+++ ETL/Enumerate.py
@@ -0,0 +1,15 @@
+#enumerate
+
+#Wrap an iterable with enumerate and it will yield the item along with its index.
+
+#For example:
+
+
+a = ['a', 'b', 'c', 'd', 'e']
+for index, item in enumerate(a): print index, item
+
+#0 a
+#1 b
+#2 c
+#3 d
+#4 e
