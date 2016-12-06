068646d67eb9af7691a83093819978d82164c9af
VossDataScience <joshvoss@outlook.com>
2016-12-06 13:02:13 -0500

    Update Youtube-Trends.py

--- Project/Youtube-Trends.py
+++ Project/Youtube-Trends.py
@@ -32,7 +32,7 @@ for v in vlist:
         response = requests.get(urlgo)
         html = response.content
         soup = BeautifulSoup(html)
-        vtitles.append(str(soup.find('h3', {'class': 'r'})) + str(', '))
+        vtitles.append(str(soup.find('h3', {'class': 'r'}).text) + str(', '))
         
 
 h3 class r
