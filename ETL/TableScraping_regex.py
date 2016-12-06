b1da940080bb569f41632a78d6886484778fb867
VossDataScience <joshvoss@outlook.com>
2016-12-05 23:59:08 -0500

    Update TableScraping_regex.py

--- ETL/TableScraping_regex.py
+++ ETL/TableScraping_regex.py
@@ -3,6 +3,19 @@ import mechanize
 import requests
 import re
 
+def rls(my_list, pattern):
+    import re
+    result = re.findall(pattern, ','.join(str(my_list)))
+    return result
+
+
+def rlsindex(my_list, pattern):
+    import re
+    r = re.findall(pattern, ','.join(str(my_list)))
+    for p in r:
+        print my_list.index(p)
+
+
 
 url= 'http://www.w3schools.com/html/html_tables.asp'
 
@@ -46,6 +59,7 @@ for r in rows:
     rowlist.append(str(', '.join(r)))
 
 
+rls('(fire)\w+', rowlist)
 
 
 
