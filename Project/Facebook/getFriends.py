c04aca1df546c9cbc9b1877adabf36126bac0473
VossDataScience <joshvoss@outlook.com>
2016-12-08 15:46:25 -0500

    Create getFriends.py

---
+++ Project/Facebook/getFriends.py
@@ -0,0 +1,27 @@
+from BeautifulSoup import BeautifulSoup
+import pandas as pd
+import numpy as np
+import mechanize
+import requests
+import sqlite3
+import re
+
+
+
+url= 'file:///C:/Users/jav11008/Desktop/Teresa%20Cramer.html'
+
+browser = mechanize.Browser()
+browser.set_handle_robots(False)
+browser.open(url)
+
+response = requests.get(browser.geturl())
+html = response.content
+soup = BeautifulSoup(html)
+
+links = soup.findAll('a', {'href': re.compile('.*(fref=pb&hc_location=friends_tab).*')})
+
+links[0]
+
+
+
+
