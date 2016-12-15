d98479184382ecffbd70c4919f43078298877b7c
VossDataScience <joshvoss@outlook.com>
2016-12-15 15:42:28 -0500

    Update and rename Commodities.py to Housing=prices.py

--- Project/Markets/Housing=prices.py
+++ Project/Markets/Housing=prices.py
@@ -6,16 +6,19 @@ import requests
 import sqlite3
 import re
 
-
-url = 'http://www.tradingeconomics.com/commodities'
+url= 'https://www.trulia.com/MI/Grand_Rapids/'
 
 browser = mechanize.Browser()
 browser.set_handle_robots(False)
 browser.open(url)
+ponse_seek_wrapper at 0x612ab20 whose wrapped object = <closeable_response at 0x612a9b8 who
+p = <socket._fileobject object at 0x0612B230>>>
 response = requests.get(browser.geturl())
 html = response.content
 soup = BeautifulSoup(html)
 
+raw_hdata = soup.findAll('div', {'class':'cardDetails man ptm phm'})
 
-ntables = int(len(soup.findAll('table')))
+for hd in raw_hdata:
+    print hd.text
 
