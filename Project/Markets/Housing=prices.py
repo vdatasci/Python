cd5802b313c129568df16ef14fc54ac0f3fa676e
VossDataScience <joshvoss@outlook.com>
2016-12-15 15:58:40 -0500

    Update Housing=prices.py

--- Project/Markets/Housing=prices.py
+++ Project/Markets/Housing=prices.py
@@ -11,8 +11,6 @@ url= 'https://www.trulia.com/MI/Grand_Rapids/'
 browser = mechanize.Browser()
 browser.set_handle_robots(False)
 browser.open(url)
-ponse_seek_wrapper at 0x612ab20 whose wrapped object = <closeable_response at 0x612a9b8 who
-p = <socket._fileobject object at 0x0612B230>>>
 response = requests.get(browser.geturl())
 html = response.content
 soup = BeautifulSoup(html)
@@ -20,5 +18,7 @@ soup = BeautifulSoup(html)
 raw_hdata = soup.findAll('div', {'class':'cardDetails man ptm phm'})
 
 for hd in raw_hdata:
-    print hd.text
+    print re.search(hd.text).group(0)
+
+
 
