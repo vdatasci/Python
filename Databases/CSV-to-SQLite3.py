8695a1012f4fadd2c1fffef56452716973dc2eb1
VossDataScience <joshvoss@outlook.com>
2016-12-27 16:18:21 -0500

    Update CSV-to-SQLite3.py

--- Databases/CSV-to-SQLite3.py
+++ Databases/CSV-to-SQLite3.py
@@ -1,7 +1,18 @@
 import sqlite3
 import csv
 
+
+conn = sqlite3.connect("db.sl3")
+curs = conn.cursor()
+
 reader = csv.reader(open('C:\\Users\\Linda\\Downloads\\Ginzberg.csv', 'r'), delimiter=',')
 
 for row in reader:
     print row
+    to_db = [unicode(row[0], "utf8"), unicode(row[1], "utf8"), unicode(row[2], "utf8")]
+    curs.execute("INSERT INTO db (type, term, definition) VALUES (?, ?, ?);", to_db)
+
+
+conn.commit()
+
+
