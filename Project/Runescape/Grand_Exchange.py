b5e8d0f102071e9089356b9d2dfe062ee1dd9878
VossDataScience <joshvoss@outlook.com>
2017-01-05 17:08:57 -0500

    Update Grand_Exchange.py

--- Project/Runescape/Grand_Exchange.py
+++ Project/Runescape/Grand_Exchange.py
@@ -2,13 +2,23 @@ from BeautifulSoup import BeautifulSoup
 import mechanize
 import requests
 import urllib2
+import sqlite3
 import json
+import csv
 import re
 
 
 url='http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=21787'
 
 response = urllib2.urlopen(url)
+data = str(response.read())
+
+
+
+
+
 
 with open('F:\_\rs\data.txt', 'w') as f:
      f.write(response.read())
+
+
