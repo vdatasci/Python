65d12db64b801e18c6af6cd7fa0bf69639410566
VossDataScience <joshvoss@outlook.com>
2017-01-06 14:30:20 -0500

    Update Grand_Exchange.py

--- Project/Runescape/Grand_Exchange.py
+++ Project/Runescape/Grand_Exchange.py
@@ -1,4 +1,6 @@
 from BeautifulSoup import BeautifulSoup
+import pandas as pd
+import numpy as np
 import mechanize
 import requests
 import urllib2
@@ -15,6 +17,15 @@ data = json.loads(str(response.read()))
 
 name = str(data['item']['name'])
 price = str(data['item']['current']['price'])
+
+
+with open('F:\\_\\rs\\items-data.csv', 'w') as f:
+     f.write(name + ', ' + price + '\n')
+
+
+
+
+
 
 df = pd.DataFrame([[name, price]])
 df.columns = ['item', 'price']
