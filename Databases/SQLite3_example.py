cbc99997ab4d301c1b3a28e16b4fbe3bb74acb27
vdatasci <joshvoss@outlook.com>
2017-01-10 08:46:04 -0500

    Update SQLite3_example.py

--- Databases/SQLite3_example.py
+++ Databases/SQLite3_example.py
@@ -52,18 +52,10 @@ tb2_Column_Names = list(map(lambda x: x[0], c.execute('''SELECT * FROM tbl2''').
 tbl2_Rows = c.execute('''SELECT * FROM tbl2''').fetchall()
 
 
-# Import PrettyTable Package
-from prettytable import PrettyTable
-
-y=PrettyTable()
-y.padding_width = 1
-y.add_column(tbl1_Column_Names[0],[row[0] for row in tbl1_Rows])
-y.add_column(tbl1_Column_Names[1],[row[1] for row in tbl1_Rows])
-y.add_column(tbl1_Column_Names[2],[format(row[2],',d') for row in tbl1_Rows])
-y.align[tbl1_Column_Names[1]]="l"
-y.align[tbl1_Column_Names[2]]="r"
-
-print(y)
+# Import Pandas Package, Print query
+Import pandas as pd
+df = pd.read_sql_query("SELECT * FROM tbl1", conn)
+print(df.head())
 
 
 ####c.execute('''PRAGMA table_info(tbl1)''').fetchall()
