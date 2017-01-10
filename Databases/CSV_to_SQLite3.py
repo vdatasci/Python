a2ac0fb5c2a37f7b8f56b41147eefa1e0a37379b
vdatasci <joshvoss@outlook.com>
2017-01-10 16:45:28 -0500

    Update CSV_to_SQLite3.py

--- Databases/CSV_to_SQLite3.py
+++ Databases/CSV_to_SQLite3.py
@@ -2,11 +2,10 @@ import pandas as pd
 import sqlite3
 
 
-csvfile_name = pd.read_csv('F:\\Data\\github\\ben519\\users.csv')
-# csvfile_name_from_internet = pd.read_csv('https://raw.githubusercontent.com/ben519/DataWrangling/master/Data/users.csv')
-
 connection = sqlite3.connect("db.sl3")
 c = conn.cursor()
+
+# csvfile_from_internet = pd.read_csv('https://raw.githubusercontent.com/ben519/DataWrangling/master/Data/users.csv')
 
 read_csv = pd.read_csv('F:\\Data\\github\\ben519\\users.csv')
 read_csv.to_sql('test1', connection)
