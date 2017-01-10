a807107068eee834d49b4432ba6fcab770046e7d
vdatasci <joshvoss@outlook.com>
2017-01-10 08:40:10 -0500

    Update SQLite3_example.py

--- Databases/SQLite3_example.py
+++ Databases/SQLite3_example.py
@@ -45,9 +45,26 @@ VALUES (?, ?, ?)
 ''', tbl1_insert_values)
 
 
-
 tbl_Column_Names = list(map(lambda x: x[0], c.execute('''SELECT * FROM tbl1''').description))
+tbl1_Rows = c.execute('''SELECT * FROM tbl1''').fetchall()
+
 tb2_Column_Names = list(map(lambda x: x[0], c.execute('''SELECT * FROM tbl2''').description))
+tbl2_Rows = c.execute('''SELECT * FROM tbl2''').fetchall()
+
+
+# Import PrettyTable Package
+from prettytable import PrettyTable
+
+y=PrettyTable()
+y.padding_width = 1
+y.add_column(tbl1_Column_Names[0],[ow[0] for row in tbl1_Rows])
+y.add_column(tbl1_Column_Names[1],[row[1] for row in tbl1_Rows])
+y.add_column(tbl1_Column_Names[2],[format(row[2],',d') for row in tbl1_Rows])
+y.align[tbl1_Column_Names[1]]="l"
+y.align[tbl1_Column_Names[2]]="r"
+
+print(y)
+
 
 ####c.execute('''PRAGMA table_info(tbl1)''').fetchall()
 
