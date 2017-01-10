897d2aa239ec3ce5f5f351fc41fca2f0f05173f4
vdatasci <joshvoss@outlook.com>
2017-01-10 16:12:49 -0500

    Create CSV_to_SQLite3.py

---
+++ Databases/CSV_to_SQLite3.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import sqlite3
+
+csvfile_name = pd.read_csv('F:\\Data\\github\\ben519\\users.csv')
+# csvfile_name_from_internet = pd.read_csv('https://raw.githubusercontent.com/ben519/DataWrangling/master/Data/users.csv')
+
+conn = sqlite3.connect("db.sl3")
+c = conn.cursor()
+
+read_csv = pd.read_csv('F:\\Data\\github\\ben519\\users.csv')
+read_csv.to_sql('test1', conn)
+
+sql_query = raw_input('Input SQL Query')
+read_sql_query = pd.read_sql_query("SELECT * FROM test1", conn)
+
+read_csv
+
