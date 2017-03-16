ef5a044d376c78ea2513184ece4e23132b2b3f0d
vdatasci <joshvoss@outlook.com>
2017-03-16 12:53:17 -0400

    Update Baby-Name-BETTER-VERSION.py

--- Project/Baby-Name-BETTER-VERSION.py
+++ Project/Baby-Name-BETTER-VERSION.py
@@ -5,10 +5,12 @@ txtdata = []
 with open('P:\\Python\\babynames2014.txt') as f:
     for line in f:
         txtdata.append(str(line.rstrip('\n')))
-        
+
+
 alldata = []
 alldata = ','.join(txtdata)
 names_array = map(lambda x:str(x), alldata.split(','))
+
 
 d = gender.Detector()
 for name in names_array:
