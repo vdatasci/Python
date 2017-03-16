f9324bc003b83ff53cd6eda405b2075045ee977a
vdatasci <joshvoss@outlook.com>
2017-03-16 14:04:16 -0400

    Create Email-Templating.py

---
+++ mail/Email-Templating.py
@@ -0,0 +1,15 @@
+from jinja2 import Environment
+import win32com.client as win32
+
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.TO = 'joshvoss90@outlook.com'
+mail.Subject = 'test'
+mail.Body = 'Message Body'
+
+TEMPLATE = '''
+<b> {{name}} </b>
+'''
+
+mail.HTMLBody = Environment().from_string(TEMPLATE).render(name='Josh').strip()
+mail.Send()
