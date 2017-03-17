0774056744c242db1b045c21cce19b4eebe0e088
vdatasci <joshvoss@outlook.com>
2017-03-17 05:09:16 -0400

    Update PrettyTables-Example.py

--- DataFrames/PrettyTables-Example.py
+++ DataFrames/PrettyTables-Example.py
@@ -1,10 +1,7 @@
 from prettytable import PrettyTable
 import csv
-import requests
-
-data = requests.get('https://raw.githubusercontent.com/vdatasci/Python/master/DataFrames/data/randomdata.txt').content
-
-with open(data,'r') as f:
+  
+with open('P:\\Python\\Data\\randomdata.txt','r') as f:
     reader = csv.DictReader(f, dialect='excel-tab')
     ptable = PrettyTable(reader.fieldnames)
     for rowdict in reader:
