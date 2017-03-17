f805a1681a14285cc52f0ded94eebc5e062b344a
vdatasci <joshvoss@outlook.com>
2017-03-17 04:38:38 -0400

    Create iss.py

---
+++ APIs/iss.py
@@ -0,0 +1,8 @@
+# Make the same request we did earlier, but with the coordinates of San Francisco instead.
+parameters = {"lat": 37.78, "lon": -122.41}
+response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+
+# Get the response data as a python object.  Verify that it's a dictionary.
+data = response.json()
+print(type(data))
+print(data)
