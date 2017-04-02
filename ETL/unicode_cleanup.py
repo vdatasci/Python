9c7536b78f8945ad25c12069eb8681586ae4b19f
vdatasci <joshvoss@outlook.com>
2017-04-02 05:21:49 -0400

    Create unicode_cleanup.py

---
+++ ETL/unicode_cleanup.py
@@ -0,0 +1,4 @@
+title = u"Klüft skräms inför på fédéral électoral große"
+import unicodedata
+unicodedata.normalize('NFKD', title).encode('ascii','ignore')
+'Kluft skrams infor pa federal electoral groe'
