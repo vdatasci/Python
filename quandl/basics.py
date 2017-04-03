bc0bd8b46b310205416f96ea4653928123bb3d23
vdatasci <joshvoss@outlook.com>
2017-04-03 14:30:31 -0400

    Update basics.py

--- quandl/basics.py
+++ quandl/basics.py
@@ -3,3 +3,7 @@ import quandl
 #Get US GDP data
 quandl.get("FRED/GDP")
 
+#Get WTI Crude Oil price from the US Department of Energy
+#(Crude Oil in Dollars per Barrel, Products in Dollars per Gallon)
+quandl.get("EIA/PET_RWTC_D")
+
