336602d0f45a81b47160f9bd017a74a249c6bfec
vdatasci <joshvoss@outlook.com>
2017-04-06 03:36:23 -0400

    Create Format.py

---
+++ ETL/Format.py
@@ -0,0 +1,8 @@
+"Hello {}, my name is {}".format('john', 'mike')
+#'Hello john, my name is mike'.
+
+"{1}, {0}".format('world', 'Hello')
+#'Hello, world'
+
+"{greeting}, {}".format('world', greeting='Hello')
+#'Hello, world'
