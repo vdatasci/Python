412a23c170038aabe09533763dd403a12403bb54
vdatasci <joshvoss@outlook.com>
2017-04-07 23:25:05 -0400

    Update Enumerate.py

--- ETL/Enumerate.py
+++ ETL/Enumerate.py
@@ -13,3 +13,6 @@ for index, item in enumerate(a): print index, item
 #2 c
 #3 d
 #4 e
+
+ranked_users = ['jon','bob','jane','alice','chris']
+user_details = [{'name':x, 'rank':i} for i,x in enumerate(ranked_users)]
