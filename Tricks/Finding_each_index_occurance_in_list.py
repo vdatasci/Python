94c886e43ee60443e8f2e044318b10dd78687a2c
vdatasci <joshvoss@outlook.com>
2017-04-08 06:55:39 -0400

    Update Finding_each_index_occurance_in_list.py

--- Tricks/Finding_each_index_occurance_in_list.py
+++ Tricks/Finding_each_index_occurance_in_list.py
@@ -1,1 +1,10 @@
 [i for i,x in enumerate('this is a sentence is this'.split(' ')) if x == 'this']
+
+
+#OR
+
+
+import numpy as np
+values = np.array([1,2,3,1,2,4,5,6,3,2,1])
+searchval = 3
+ii = np.where(values == searchval)[0]
