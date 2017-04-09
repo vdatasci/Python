7d04643132136caba9e16c409ddbda8a40dae6ae
vdatasci <joshvoss@outlook.com>
2017-04-08 22:53:20 -0400

    Update SQLAlchemy-example1.py

--- Databases/SQLAlchemy-example1.py
+++ Databases/SQLAlchemy-example1.py
@@ -7,3 +7,6 @@ csv_url = 'http://samplecsvs.s3.amazonaws.com/SalesJan2009.csv'
 df = pd.read_csv(csv_url)
 
 df.to_sql('table1', engine)
+pd.read_sql_query('''SELECT Transaction_date,Name,Last_Login FROM table1''', engine)
+
+pd.read_sql_query('''SELECT Transaction_date,Name,Last_Login FROM table1 WHERE Last_Login >= '2/9/09' ORDER BY Last_Login DESC''', engine)
