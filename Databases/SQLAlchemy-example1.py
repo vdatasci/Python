2bce25265fd387ce8239e9728d645c8e5fd5fa6b
vdatasci <joshvoss@outlook.com>
2017-04-08 22:46:41 -0400

    Update SQLAlchemy-example1.py

--- Databases/SQLAlchemy-example1.py
+++ Databases/SQLAlchemy-example1.py
@@ -4,5 +4,6 @@ from sqlalchemy import create_engine
 engine = create_engine('sqlite:///:memory:')
 
 csv_url = 'http://samplecsvs.s3.amazonaws.com/SalesJan2009.csv'
-
 df = pd.read_csv(csv_url)
+
+df.to_sql('table1', engine)
