3015329fbab2db2b9db7444835acbae289a521e3
vdatasci <joshvoss@outlook.com>
2017-04-08 22:45:05 -0400

    Create SQLAlchemy-example1.py

---
+++ Databases/SQLAlchemy-example1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///:memory:')
+
+csv_url = 'http://samplecsvs.s3.amazonaws.com/SalesJan2009.csv'
+
+df = pd.read_csv(csv_url)
