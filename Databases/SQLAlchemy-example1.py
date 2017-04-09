7d696dff820883fc050396b74f6039af91e50008
vdatasci <joshvoss@outlook.com>
2017-04-08 23:11:03 -0400

    Update SQLAlchemy-example1.py

--- Databases/SQLAlchemy-example1.py
+++ Databases/SQLAlchemy-example1.py
@@ -7,9 +7,17 @@ csv_url = 'http://samplecsvs.s3.amazonaws.com/SalesJan2009.csv'
 df = pd.read_csv(csv_url)
 
 df.to_sql('table1', engine)
-pd.read_sql_query('''SELECT Transaction_date,Name,Last_Login FROM table1''', engine)
 
-pd.read_sql_query('''SELECT Transaction_date,Name,Last_Login FROM table1 WHERE Last_Login >= '2/9/09' ORDER BY Last_Login DESC''', engine)
+qdf = pd.read_sql_query('''SELECT Transaction_date,Name,Last_Login FROM table1 WHERE Last_Login >= '2/9/09' ORDER BY Last_Login DESC''', engine)
 
 qdf[(qdf['Name'] == 'julie')]
 
+
+
+lookups = ["julie","julia"]
+
+choices = qdf.set_index("Transaction_date").Name.to_dict()
+
+res = [(lookup,) + item for lookup in lookups for item in process.extract(lookup, choices, limit=2)]
+rdf = pd.DataFrame(res, columns=["lookup", "matched", "score", "id"])
+rdf
