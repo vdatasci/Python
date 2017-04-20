6b810bd7fc5998c28d0a3d48515fae0961a019cf
vdatasci <joshvoss@outlook.com>
2017-04-20 15:02:25 -0400

    Create Nooz.py

---
+++ Project/Googlr/Nooz.py
@@ -0,0 +1,35 @@
+from BeautifulSoup import BeautifulSoup
+import pandas as pd
+import numpy as np
+import mechanize
+import requests
+import urllib2
+import re
+
+
+goos = str('grand rapids michigan')
+
+#goos = raw_input('Search Google:   ')
+
+
+url = 'https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=' + goos.replace(' ', '+') + str('&tbm=nws')
+
+
+response = requests.get(str(url))
+html = response.content
+soup = BeautifulSoup(html)
+
+response.url
+
+
+try:
+    NId = soup.find('div', {'class': '_NId'})
+    ires = soup.find('div', {'id':'ires'})
+except AttributeError:
+    continue
+
+
+#price = str(soup.find('span', {'title': re.compile('\d+')}).text)
+#description = str(soup.find('div', {'class': 'item-description'}).findChild('p').text)
+
+NId
