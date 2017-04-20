83aa94fcb980d447987d7711ef400090be5b3af9
vdatasci <joshvoss@outlook.com>
2017-04-20 00:04:46 -0400

    Update Googlr.py

--- Project/Googlr/Googlr.py
+++ Project/Googlr/Googlr.py
@@ -7,7 +7,22 @@ import urllib2
 import re
 
 
-url = 'https://www.google.com'
+sgoo = raw_input('Search Google:   ')
+
+url = 'https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q='+sgoo.replace(' ', '+')
+
+
 response = requests.get(str(url))
 html = response.content
 soup = BeautifulSoup(html)
+
+response.url
+
+try:
+    name = str(soup.find('div', {'class': 'item-description'}).findChild('h2').text)
+    price = str(soup.find('span', {'title': re.compile('\d+')}).text)
+    description = str(soup.find('div', {'class': 'item-description'}).findChild('p').text)
+    new_row = np.array((name + ',' + price + ',' + description).split(','))
+    items_list = np.vstack((items_list,new_row))
+except AttributeError:
+    continue
