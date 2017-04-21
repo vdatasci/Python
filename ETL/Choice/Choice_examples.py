1042a763ca4c4bfbf17bb70ee9eeea2783ef666f
vdatasci <joshvoss@outlook.com>
2017-04-21 00:14:03 -0400

    Update Choice_examples.py

--- ETL/Choice/Choice_examples.py
+++ ETL/Choice/Choice_examples.py
@@ -4,3 +4,16 @@ possible_destinations = ["Berlin", "Hamburg", "Munich",
                          "Zurich", "Heidelberg", "Strasbourg", 
                          "Augsburg", "Milan", "Rome"]
 print(choice(possible_destinations))
+
+
+from numpy.random import choice
+print(choice(possible_destinations))
+
+
+x1 = choice(possible_destinations, size=3)
+print(x1)
+x2 = choice(possible_destinations, size=(3, 4))
+print(x2)
+
+
+print(choice(possible_destinations, size=(3, 4), replace=False))
