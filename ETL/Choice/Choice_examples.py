4635c453e6db44759d5fdd09e0a311c96a4c4b53
vdatasci <joshvoss@outlook.com>
2017-04-21 00:12:33 -0400

    Create Choice_examples.py

---
+++ ETL/Choice/Choice_examples.py
@@ -0,0 +1,6 @@
+from random import choice
+possible_destinations = ["Berlin", "Hamburg", "Munich", 
+                         "Amsterdam", "London", "Paris", 
+                         "Zurich", "Heidelberg", "Strasbourg", 
+                         "Augsburg", "Milan", "Rome"]
+print(choice(possible_destinations))
