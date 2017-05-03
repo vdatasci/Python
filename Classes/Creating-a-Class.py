a2f56a9614fce588806b35e5fb9376c2666da6f8
vdatasci <joshvoss@outlook.com>
2017-05-03 17:18:59 -0400

    Update Creating-a-Class.py

--- Classes/Creating-a-Class.py
+++ Classes/Creating-a-Class.py
@@ -3,5 +3,9 @@ class stock:
     def __init__(self, name, price):
             self.name = name
             self.price = price
+    def __getitem__(self, i):
+        return self.name[i]
+    def __iter__(self):
+        return self.name.itervalues()
 
 x = stock('VOS', 234)
