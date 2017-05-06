b2f2fd94dd970fa5dde2d45ebae22e583940115d
vdatasci <joshvoss@outlook.com>
2017-05-06 00:50:48 -0400

    Update Linear_Regression2.py

--- Quantitative-Economics/Linear-Algebra/Linear_Regression2.py
+++ Quantitative-Economics/Linear-Algebra/Linear_Regression2.py
@@ -3,14 +3,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# X and Y data points as two different arrays (x,y)
 x = [1,2,3,4]
 y = [3,5,7,10]
 
+# fit_fn is a function which takes x and returns an estimate for y
 fit = np.polyfit(x,y,1)
 fit_fn = np.poly1d(fit) 
-# fit_fn is now a function which takes in x and returns an estimate for y
 
-#Chart formatting
+
+# Chart formatting
+# https://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.plot
 plt.plot(x,y, 'yo', x, fit_fn(x), '--k')
 plt.xlim(0, 5)
 plt.ylim(0, 12)
