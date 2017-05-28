9a65ae7ccca778a937cf757513420441349c2c86
vdatasci <joshvoss90@outlook.com>
2017-05-27 21:12:03 -0400

    Update iss.py

--- APIs/iss.py
+++ APIs/iss.py
@@ -1,3 +1,5 @@
+import requests
+
 # Make the same request we did earlier, but with the coordinates of San Francisco instead.
 parameters = {"lat": 37.78, "lon": -122.41}
 response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
